793a240e8a5a5755c5a7d7565179f7c9cbbf5ca5
Carlos Castro <contato-carloscastro@hotmail.com>
2021-05-17 15:21:05 -0300

    Remove comentários desnecessários

--- App.tsx
+++ App.tsx
@@ -1,13 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import theme from 'infra/theme';
 import React from 'react';
 import {SafeAreaView} from 'react-native';
