66c16561d79943c2fe62ada38aa704157d3981a5
Carlos Castro <contato-carloscastro@hotmail.com>
2021-05-17 16:08:39 -0300

    renomeia teste

--- __tests__/component/App.spec.tsx
+++ __tests__/component/App.spec.tsx
@@ -9,6 +9,8 @@ import App from '../../App';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+describe('<App />', () => {
+  it('O componente renderiza corretamente.', () => {
+    renderer.create(<App />);
+  });
 });
