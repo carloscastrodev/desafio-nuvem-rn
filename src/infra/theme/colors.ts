9f9e8df20cfabd603ebc42c3f660d2ede103ae22
Carlos Castro <contato-carloscastro@hotmail.com>
2021-05-17 14:29:23 -0300

    configurações de tema para o react-native-paper

---
+++ src/infra/theme/colors.ts
@@ -0,0 +1,13 @@
+const red = '#F52626';
+const white = '#FFFFFF';
+const lightGray = '#00000029';
+const gray = '#898383';
+const darkGray = '#00000077';
+
+export default {
+  red,
+  white,
+  lightGray,
+  gray,
+  darkGray,
+};
---
+++ src/infra/theme/index.ts
@@ -0,0 +1,57 @@
+import {Platform} from 'react-native';
+import {DefaultTheme} from 'react-native-paper';
+import {Theme} from 'react-native-paper/lib/typescript/types';
+import colors from './colors';
+
+const theme: Theme = {
+  ...DefaultTheme,
+  colors: {
+    error: colors.red,
+    accent: colors.gray,
+    backdrop: colors.lightGray,
+    background: colors.white,
+    disabled: colors.gray,
+    notification: colors.red,
+    onSurface: colors.darkGray,
+    primary: colors.gray,
+    surface: colors.white,
+    placeholder: colors.lightGray,
+    text: colors.darkGray,
+  },
+  fonts: {
+    regular: {
+      fontFamily:
+        Platform.select({
+          ios: 'Helvetica Neue',
+          android: 'sans-serif',
+        }) || 'sans-serif',
+      fontWeight: 'normal',
+    },
+    light: {
+      fontFamily:
+        Platform.select({
+          ios: 'HelveticaNeue-Light',
+          android: 'sans-serif-light',
+        }) || 'sans-serif',
+      fontWeight: 'normal',
+    },
+    medium: {
+      fontFamily:
+        Platform.select({
+          ios: 'HelveticaNeue-Medium',
+          android: 'sans-serif-medium',
+        }) || 'sans-serif-medium',
+      fontWeight: 'normal',
+    },
+    thin: {
+      fontFamily:
+        Platform.select({
+          ios: 'HelveticaNeue-Thin',
+          android: 'sans-serif-thin',
+        }) || 'sans-serif-thin',
+      fontWeight: 'normal',
+    },
+  },
+};
+
+export default theme;
