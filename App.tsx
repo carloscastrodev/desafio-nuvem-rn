dab276202ae3515b83c2266ce6d66d83e81ec130
Carlos Castro <contato-carloscastro@hotmail.com>
2021-05-18 12:15:43 -0300

    Remove <SafeAreaView> errado

--- App.tsx
+++ App.tsx
@@ -1,15 +1,12 @@
 import theme from 'infra/theme';
 import React from 'react';
-import {SafeAreaView} from 'react-native';
 import {Provider as PaperProvider} from 'react-native-paper';
 import Screens from 'ui/screens';
 
 const App = () => {
   return (
     <PaperProvider theme={theme}>
-      <SafeAreaView>
-        <Screens />
-      </SafeAreaView>
+      <Screens />
     </PaperProvider>
   );
 };
