9fdf5f4d0e48aaee9dd92e034393ba9c38d65ae2
Carlos Castro <contato-carloscastro@hotmail.com>
2021-05-18 13:21:38 -0300

    Atualiza interface com campos opcionais

--- src/infra/types/Taxi.d.ts
+++ src/infra/types/Taxi.d.ts
@@ -1,6 +1,12 @@
+import {ImageURISource} from 'react-native';
+
 export interface Taxi {
   id: number;
   nome: string;
   latitude: number;
   longitude: number;
+  imageUri?: ImageURISource;
+  precoKm?: number;
+  limiteKm?: number;
+  kmRodado?: number;
 }
