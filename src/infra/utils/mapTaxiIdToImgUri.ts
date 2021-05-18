79f17e1f5fa8c0f09f2d4045665d50f6a88374b9
Carlos Castro <contato-carloscastro@hotmail.com>
2021-05-18 18:09:13 -0300

    adiciona util para mapear a imagem para o id do taxi

---
+++ src/infra/utils/mapTaxiIdToImgUri.ts
@@ -0,0 +1,28 @@
+import jamesImg from 'assets/images/james.jpeg';
+import karlaImg from 'assets/images/karla.jpeg';
+import leticiaImg from 'assets/images/leticia.png';
+import luizaImg from 'assets/images/luiza.jpeg';
+import marioImg from 'assets/images/mario.jpeg';
+import matheusImg from 'assets/images/matheus.jpeg';
+import {ImageSourcePropType} from 'react-native';
+
+const mapTaxiIdToImgUri = (id: number): ImageSourcePropType | undefined => {
+  switch (id) {
+    case 1:
+      return leticiaImg;
+    case 2:
+      return marioImg;
+    case 3:
+      return luizaImg;
+    case 4:
+      return jamesImg;
+    case 5:
+      return karlaImg;
+    case 6:
+      return matheusImg;
+    default:
+      return undefined;
+  }
+};
+
+export default mapTaxiIdToImgUri;
