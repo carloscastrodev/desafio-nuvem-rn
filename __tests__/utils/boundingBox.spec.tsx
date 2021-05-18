786bbcca6f857fb7620417115b5798c265195ff3
Carlos Castro <contato-carloscastro@hotmail.com>
2021-05-18 13:20:55 -0300

    Adicionando mais testes

--- __tests__/utils/boundingBox.spec.tsx
+++ __tests__/utils/boundingBox.spec.tsx
@@ -1,16 +1,16 @@
 import boundingBox from 'infra/utils/boundingBox';
 
-const mockCoords: [number, number][] = [
-  [0, 0],
-  [-1, 0],
-  [-1, -1],
-  [1, 1],
-  [1, 2],
-  [2, 2],
-];
-
 describe('boundingBox()', () => {
-  it('Deve retornar as coordenadas das fronteiras corretamente.', () => {
+  it('Deve retornar as coordenadas das fronteiras corretamente com mais de um ponto.', () => {
+    const mockCoords: [number, number][] = [
+      [0, 0],
+      [-1, 0],
+      [-1, -1],
+      [1, 1],
+      [1, 2],
+      [2, 2],
+    ];
+
     const _boundingBox = boundingBox(mockCoords);
     const testBoundingBox: BoundingBox = {
       minLat: -1,
@@ -21,5 +21,28 @@ describe('boundingBox()', () => {
     };
 
     expect(_boundingBox).toEqual(testBoundingBox);
+  });
+
+  it('Deve retornar as coordenadas das fronteiras corretamente com apenas um ponto.', () => {
+    const mockCoords: [number, number][] = [[-1, -1]];
+
+    const _boundingBox = boundingBox(mockCoords);
+    const testBoundingBox: BoundingBox = {
+      minLat: -1,
+      minLong: -1,
+      maxLat: -1,
+      maxLong: -1,
+      center: [-1, -1],
+    };
+
+    expect(_boundingBox).toEqual(testBoundingBox);
+  });
+
+  it('Deve lançar um erro quando não houver nenhum ponto.', () => {
+    const mockCoords: [number, number][] = [];
+
+    const _boundingBox = () => boundingBox(mockCoords);
+
+    expect(_boundingBox).toThrow(Error);
   });
 });
