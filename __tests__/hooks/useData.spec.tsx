abcb7dfb7b5f0d609f509e389d3570433949c5f5
Carlos Castro <contato-carloscastro@hotmail.com>
2021-05-18 18:13:05 -0300

    adiciona espera para atualização do estado do hook antes de finalizar o teste

--- __tests__/hooks/useData.spec.tsx
+++ __tests__/hooks/useData.spec.tsx
@@ -1,15 +1,16 @@
 import 'react-native';
-import useData from 'infra/hooks/useData';
+import useTaxis from 'infra/hooks/useTaxis';
 import {
   Renderer,
   renderHook,
   RenderHookResult,
 } from '@testing-library/react-hooks';
 import {Taxi} from 'infra/types/Taxi';
+import {waitFor} from '@testing-library/react-native';
 
 // Note: test renderer must be required after react-native.
 
-describe('useData()', () => {
+describe('useTaxis()', () => {
   let hookResult: RenderHookResult<
     unknown,
     {
@@ -20,16 +21,22 @@ describe('useData()', () => {
   >;
 
   beforeEach(() => {
-    hookResult = renderHook(() => useData());
+    hookResult = renderHook(() => useTaxis());
   });
 
-  it('Retorna uma lista vazia inicialmente', () => {
+  it('Retorna uma lista de elementos.', async () => {
     const {data} = hookResult.result.current;
     expect(data).toHaveLength(0);
+    await waitFor(() =>
+      expect(hookResult.result.current.data.length).toBeGreaterThan(0),
+    );
   });
 
-  it('Retorna o estado de carregamento', () => {
+  it('Retorna o estado de carregamento.', async () => {
     const {isLoading} = hookResult.result.current;
     expect(isLoading).toBeTruthy();
+    await waitFor(() =>
+      expect(hookResult.result.current.isLoading).toBeFalsy(),
+    );
   });
 });
