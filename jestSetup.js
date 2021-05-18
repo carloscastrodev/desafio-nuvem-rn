9b13d813c710addadfe45195480f5d959cded049
Carlos Castro <contato-carloscastro@hotmail.com>
2021-05-18 12:10:26 -0300

    Adiciona mock do react-native-maps para testes

--- jestSetup.js
+++ jestSetup.js
@@ -1,3 +1,23 @@
 /* eslint-disable no-undef */
 jest.useFakeTimers();
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+
+jest.mock('react-native-maps', () => {
+  const {View} = require('react-native');
+  const React = require('react');
+  class MockMapView extends React.Component {
+    render() {
+      return <View>{this.props.children}</View>;
+    }
+  }
+  class MockMarker extends React.Component {
+    render() {
+      return <View>{this.props.children}</View>;
+    }
+  }
+  return {
+    __esModule: true,
+    default: MockMapView,
+    Marker: MockMarker,
+  };
+});
