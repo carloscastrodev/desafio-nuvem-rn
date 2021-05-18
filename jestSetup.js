3babcb548bf130d934e6afab6aaf641d9f9fd746
Carlos Castro <contato-carloscastro@hotmail.com>
2021-05-18 18:06:39 -0300

    remove utilização de fakeTimers, aiciona mock do async storage

--- jestSetup.js
+++ jestSetup.js
@@ -1,18 +1,22 @@
 /* eslint-disable no-undef */
-jest.useFakeTimers();
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+
+jest.mock('@react-native-async-storage/async-storage', () => {
+  const mockAsyncStorage = require('@react-native-async-storage/async-storage/jest/async-storage-mock');
+  return mockAsyncStorage;
+});
 
 jest.mock('react-native-maps', () => {
   const {View} = require('react-native');
   const React = require('react');
   class MockMapView extends React.Component {
     render() {
-      return <View>{this.props.children}</View>;
+      return <View {...this.props}>{this.props.children}</View>;
     }
   }
   class MockMarker extends React.Component {
     render() {
-      return <View>{this.props.children}</View>;
+      return <View {...this.props}>{this.props.children}</View>;
     }
   }
   return {
