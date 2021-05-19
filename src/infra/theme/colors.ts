01c7d2361605fb545163b07144ddd3a186d260cc
Carlos Castro <contato-carloscastro@hotmail.com>
2021-05-19 18:21:41 -0300

    adiciona mais cores ao tema

--- src/infra/theme/colors.ts
+++ src/infra/theme/colors.ts
@@ -1,12 +1,16 @@
 const red = '#F52626';
+const blue = '#188FD9';
 const white = '#FFFFFF';
+const whiteAccent = '#F7F7F7';
 const lightGray = '#00000029';
 const gray = '#898383';
 const darkGray = '#00000077';
 
 export default {
   red,
+  blue,
   white,
+  whiteAccent,
   lightGray,
   gray,
   darkGray,
