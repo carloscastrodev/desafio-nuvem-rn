715969ea58ad6fba190ab2b2eb7fe34c48ff9ab4
Carlos Castro <contato-carloscastro@hotmail.com>
2021-05-19 18:23:17 -0300

    finalizando o componente de detalhes do taxi

--- src/ui/components/TaxiDetail/index.tsx
+++ src/ui/components/TaxiDetail/index.tsx
@@ -1,39 +1,66 @@
-import React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
-import Modal, {OnSwipeCompleteParams} from 'react-native-modal';
-
+import React, {useEffect} from 'react';
+import {Avatar, Button} from 'react-native-paper';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  KeyboardAvoidingView,
+  Alert,
+} from 'react-native';
+import Modal from 'react-native-modal';
+import NumericInput from 'react-native-numeric-input';
 import {Taxi} from 'infra/types/Taxi';
-
+import carIcon from 'assets/images/car_icon.png';
 import styles from './styles';
-import {Avatar} from 'react-native-paper';
-import {useCallback} from 'react';
+import {useState} from 'react';
+import {actionButtonTheme} from 'infra/theme';
+import formatCoin from 'infra/utils/formatCoin';
 
 interface ComponentProps {
   isVisible: boolean;
   taxi: Taxi;
+  onSubmit: (taxi: Taxi, distance: number) => void;
   onClose: () => void;
 }
 
-function TaxiDetail({isVisible, taxi, onClose}: ComponentProps) {
-  const handleSwipeBottom = useCallback(
-    (ev: OnSwipeCompleteParams) => {
-      if (ev.swipingDirection === 'down') {
-        onClose();
-      }
-    },
-    [onClose],
-  );
+function TaxiDetail({isVisible, taxi, onClose, onSubmit}: ComponentProps) {
+  const [distance, setDistance] = useState<number>(0);
+
+  useEffect(() => {
+    setDistance(0);
+  }, [taxi]);
+
+  const remainingKM = () => {
+    if (Number.isFinite(taxi.limiteKm) && Number.isFinite(taxi.kmRodado)) {
+      return (taxi.limiteKm as number) - (taxi.kmRodado as number);
+    }
+    return 0;
+  };
+
+  const updateDistance = (d: number) => {
+    if (d <= remainingKM() && d >= 0) {
+      setDistance(d);
+    } else {
+      setDistance(remainingKM());
+      Alert.alert(
+        'Erro',
+        'Escolha uma distância maior que 0 e menor que o limite restante.',
+      );
+    }
+  };
 
   return (
     <Modal
+      testID="taxi-detail-modal"
       isVisible={isVisible}
       style={styles.wrapper}
       hasBackdrop={true}
       onDismiss={onClose}
-      onSwipeComplete={handleSwipeBottom}
-      onBackdropPress={onClose}
+      onSwipeComplete={onClose}
+      swipeDirection="down"
       backdropOpacity={0}>
-      <View style={styles.container}>
+      <KeyboardAvoidingView style={styles.container}>
         <View style={styles.headerMenu}>
           <TouchableOpacity
             style={styles.closeButton}
@@ -43,9 +70,83 @@ function TaxiDetail({isVisible, taxi, onClose}: ComponentProps) {
           </TouchableOpacity>
         </View>
         <View style={styles.content}>
-          <Text>{taxi.nome}</Text>
+          <View style={styles.infoHeader}>
+            <View style={styles.personInfo}>
+              <View style={styles.avatarImageWrapper}>
+                {taxi.imageUri ? (
+                  <Image source={taxi.imageUri} style={styles.avatar} />
+                ) : null}
+              </View>
+              <Text style={[styles.importantText, styles.personName]}>
+                {taxi.nome}
+              </Text>
+            </View>
+
+            {taxi.precoKm ? (
+              <Text style={styles.priceInfo} testID="price-km">
+                {formatCoin(taxi.precoKm)}
+                /km
+              </Text>
+            ) : (
+              <View />
+            )}
+
+            <Image source={carIcon} style={styles.carIcon} />
+          </View>
+
+          <View style={styles.section}>
+            <Text style={styles.label}>Distância da Viagem em KM</Text>
+            <NumericInput
+              minValue={0}
+              maxValue={remainingKM()}
+              rounded
+              totalWidth={180}
+              totalHeight={45}
+              step={0.1}
+              valueType="real"
+              initValue={distance}
+              containerStyle={styles.kmInput}
+              editable={remainingKM() > 0}
+              type={undefined}
+              onChange={updateDistance}
+              extraTextInputProps={{
+                testID: 'input-km',
+              }}
+            />
+          </View>
+
+          <View style={styles.section}>
+            <Text style={styles.label}>KM ainda disponível</Text>
+            {Number.isFinite(taxi.limiteKm) &&
+            Number.isFinite(taxi.kmRodado) ? (
+              <Text style={styles.importantText} testID="remaining-km">
+                {remainingKM() - distance} KM
+              </Text>
+            ) : null}
+          </View>
+
+          <View style={styles.section}>
+            <Text style={styles.label}>Total</Text>
+            {Number.isFinite(taxi.precoKm) ? (
+              <Text style={styles.importantText} testID="total-price">
+                {formatCoin((taxi.precoKm as number) * distance)}
+              </Text>
+            ) : null}
+          </View>
+
+          <Button
+            mode="contained"
+            testID="submit-button"
+            uppercase={false}
+            compact
+            labelStyle={styles.submitButtonLabel}
+            theme={actionButtonTheme}
+            style={styles.submitButton}
+            onPress={() => onSubmit(taxi, distance)}>
+            Fazer viagem
+          </Button>
         </View>
-      </View>
+      </KeyboardAvoidingView>
     </Modal>
   );
 }
--- src/ui/components/TaxiDetail/styles.ts
+++ src/ui/components/TaxiDetail/styles.ts
@@ -4,17 +4,18 @@ import {StyleSheet} from 'react-native';
 const styles = StyleSheet.create({
   wrapper: {
     margin: 0,
-    position: 'absolute',
-    width: '100%',
-    height: '84%',
-    bottom: 0,
-    backgroundColor: colors.white,
-    borderTopLeftRadius: 32,
-    borderTopRightRadius: 32,
+    ...StyleSheet.absoluteFillObject,
   },
   container: {
+    flex: 0,
     padding: 8,
-    ...StyleSheet.absoluteFillObject,
+    width: '100%',
+    height: '84%',
+    backgroundColor: colors.white,
+    zIndex: 2,
+    marginTop: 'auto',
+    borderTopLeftRadius: 32,
+    borderTopRightRadius: 32,
   },
   headerMenu: {
     flexDirection: 'row',
@@ -28,6 +29,59 @@ const styles = StyleSheet.create({
   },
   content: {
     flex: 1,
+    paddingHorizontal: 16,
+    justifyContent: 'space-between',
+  },
+  infoHeader: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  personInfo: {
+    alignItems: 'center',
+  },
+  personName: {
+    marginTop: 10,
+  },
+  avatarImageWrapper: {
+    width: 120,
+    height: 120,
+    borderRadius: 50,
+    overflow: 'hidden',
+  },
+  avatar: {
+    resizeMode: 'contain',
+    width: '100%',
+    height: '100%',
+    borderRadius: 50,
+  },
+  priceInfo: {
+    fontSize: 16,
+  },
+  carIcon: {height: 40, width: 70, resizeMode: 'contain'},
+  kmInput: {
+    marginTop: 8,
+  },
+  submitButton: {
+    width: '100%',
+    marginTop: 24,
+    padding: 0,
+    height: 50,
+    justifyContent: 'center',
+  },
+  submitButtonLabel: {
+    width: '100%',
+    height: 50,
+    textAlignVertical: 'center',
+  },
+  importantText: {
+    fontSize: 18,
+  },
+  label: {
+    fontSize: 14,
+  },
+  section: {
+    marginTop: 24,
   },
 });
 
